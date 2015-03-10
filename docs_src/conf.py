5e395d727821a7879d93f9fac7f77939cd997eab
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2015-03-10 10:25:20 -0400

    Read the Docs

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # Read the docs configuration.
 if os.environ.get("READTHEDOCS", None) == "True":
     import mock
-    MOCK_MODULES = ['numpy', "matplotlib"]
+    MOCK_MODULES = ['numpy', "matplotlib", "matplotlib.pyplot"]
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = mock.Mock()
 
