467c7592010ced719683c7553a7e2f3b6c729493
Louis-Philippe Lemieux Perreault <louis-philippe.lemieux.perreault@statgen.org>
2024-12-09 12:01:55 -0500

    Code uniformization

--- pygenclean/tests/qc_modules/test_marker_call_rate.py
+++ pygenclean/tests/qc_modules/test_marker_call_rate.py
@@ -53,9 +53,9 @@ def test_main_no_variants_removed(tmp_path: Path):
 def test_main_variants_removed(tmp_path: Path):
     """Test the main function (variants were removed).
 
-    There will be two variants with missing rate high enough, and one variant
-    with missing rate equals to the 'geno' value. This is to check if the
-    threshold is inclusiive or not.
+    There will be multiple variants with missing rate high enough, and one
+    variant with missing rate equals to the 'geno' value. This is to check if
+    the threshold is inclusiive or not.
 
     """
     # Generating the genotypes
@@ -92,9 +92,7 @@ def test_main_variants_removed(tmp_path: Path):
     # The '.removed_snps' file should have 9 variants
     with open(out + ".removed_snps") as f:
         removed_variants = set(f.read().splitlines())
-        assert removed_variants == {
-            f"var{i + 1}" for i in indices[:-1]
-        }
+        assert removed_variants == {f"var{i + 1}" for i in indices[:-1]}
 
     # The FAM files should be identical
     with open(prefix + ".fam", "rb") as f1, open(out + ".fam", "rb") as f2:
